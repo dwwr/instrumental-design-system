c82759ecad94abc381f6b0cd0d9abf14d75942ce
dwwr <dwwarnerreyes@gmail.com>
2026-09-07 22:25:08 -0700

    update r2 stories

--- src/components/readout-2/molecules/hexagon/Hexagon.stories.tsx
+++ src/components/readout-2/molecules/hexagon/Hexagon.stories.tsx
@@ -3,88 +3,80 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Hexagon } from './Hexagon'
 import { containerStyle, mainStyle } from '../../styles'
 
+const variants: { name: string; isOn: boolean; outline: boolean }[] = [
+  { name: 'On', isOn: true, outline: false },
+  { name: 'Off With Outline', isOn: false, outline: true },
+  { name: 'Off Without Outline', isOn: false, outline: false },
+]
+
 const meta: Meta<typeof Hexagon> = {
   title: 'Components/Readout2/Hexagon',
   component: Hexagon,
-  argTypes: {
-    isOn: { control: 'boolean' },
+  parameters: {
+    layout: 'fullscreen',
   },
-  render: args => (
-    <div
-      css={mainStyle}
-      style={{
-        width: '130px',
-        height: '90px',
-        boxSizing: 'border-box',
-        display: 'flex',
-        alignItems: 'start',
-        justifyContent: 'start',
-        marginLeft: '-1.75rem',
-      }}
-    >
-      <div css={containerStyle} data-outline={args.isOn ? 'false' : 'true'}>
-        <Hexagon {...args} />
-      </div>
-    </div>
-  ),
 }
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const On: Story = {
-  args: {
-    isOn: true,
-    text: 'Emergency',
-  },
-}
-
-export const OffWithOutline: Story = {
-  args: {
-    isOn: false,
-    text: 'Emergency',
-  },
-  render: args => (
+export const Stories: Story = {
+  render: () => (
     <div
-      css={mainStyle}
       style={{
-        width: '130px',
-        height: '90px',
+        width: '100%',
+        minHeight: '100vh',
+        backgroundColor: 'black',
         boxSizing: 'border-box',
+        padding: '2rem',
         display: 'flex',
-        alignItems: 'start',
-        justifyContent: 'start',
-        marginLeft: '-1.75rem',
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        gap: '0.75rem',
       }}
     >
-      <div css={containerStyle} data-outline="true">
-        <Hexagon {...args} />
-      </div>
-    </div>
-  ),
-}
-
-export const OffWithoutOutline: Story = {
-  args: {
-    isOn: false,
-    text: 'Emergency',
-  },
-  render: args => (
-    <div
-      css={mainStyle}
-      style={{
-        width: '130px',
-        height: '90px',
-        boxSizing: 'border-box',
-        display: 'flex',
-        alignItems: 'start',
-        justifyContent: 'start',
-        marginLeft: '-1.75rem',
-      }}
-    >
-      <div css={containerStyle} data-outline="false">
-        <Hexagon {...args} />
-      </div>
+      {variants.map(({ name, isOn, outline }) => (
+        <div
+          key={name}
+          style={{
+            flex: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            minWidth: 0,
+          }}
+        >
+          <div
+            style={{
+              color: 'rgba(255, 255, 255, 0.55)',
+              fontFamily: 'Helvetica, sans-serif',
+              fontSize: '0.75rem',
+              letterSpacing: '0.04em',
+              textTransform: 'uppercase',
+              marginBottom: '0.35rem',
+              textAlign: 'center',
+            }}
+          >
+            {name}
+          </div>
+          <div
+            css={mainStyle}
+            style={{
+              width: '130px',
+              height: '90px',
+              boxSizing: 'border-box',
+              display: 'flex',
+              alignItems: 'start',
+              justifyContent: 'start',
+              marginLeft: '-1.75rem',
+            }}
+          >
+            <div css={containerStyle} data-outline={outline ? 'true' : 'false'}>
+              <Hexagon isOn={isOn} text="Emergency" />
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   ),
 }
